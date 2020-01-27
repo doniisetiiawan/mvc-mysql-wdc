a8c44a8bfcaf542f5c7db36ab8d56cf649983856
Doni Setiawan <donisetiawan.050@gmail.com>
2020-01-27 15:58:09 +0700

    ...

---
+++ server/models/band.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Band = sequelize.define(
+    'Band',
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      album: DataTypes.STRING,
+      year: DataTypes.STRING,
+      UserId: DataTypes.INTEGER,
+    },
+    {},
+  );
+  Band.associate = (models) => {
+    // associations can be defined here
+    Band.belongsTo(models.User);
+  };
+  return Band;
+};
