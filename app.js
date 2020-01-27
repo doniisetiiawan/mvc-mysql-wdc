f23e78ee59ec1e008a14540fb903077af44f16dc
Doni Setiawan <donisetiawan.050@gmail.com>
2020-01-27 13:49:12 +0700

    Creating a controllers folder

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const index = require('./server/controllers/index');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', index.show);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
